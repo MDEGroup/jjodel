58e6a59e95247e5fc9e0fb1e1d5c636549cf5dbb
Damiano Di Vincenzo <naraku7686@live.it>
2024-04-26 14:21:48 +0200

    anchor started

--- src/joiner/classes.ts
+++ src/joiner/classes.ts
@@ -803,18 +803,21 @@ export class Constructors<T extends DPointerTargetable = DPointerTargetable>{
     DVertex(): this { return this; }
     DVoidEdge(start: DGraphElement["id"] | DGraphElement | LGraphElement | DModelElement["id"] | DModelElement | LModelElement,
           end: DGraphElement["id"] | DGraphElement | LGraphElement | DModelElement["id"] | DModelElement | LModelElement,
-          longestLabel: DEdge["longestLabel"], labels: DEdge["labels"]): this {
+          longestLabel?: DEdge["longestLabel"], labels?: DEdge["labels"]): this {
         const thiss: DVoidEdge = this.thiss as any;
         let startid: DGraphElement["id"] = (windoww.LGraphElement as typeof LGraphElement).getNodeId(start);
         let endid: DGraphElement["id"] = (windoww.LGraphElement as typeof LGraphElement).getNodeId(end);
         Log.ex(!startid || !endid, "cannot create an edge without start or ending nodes", {start, end, startid, endid});
         thiss.midnodes = [];
+        thiss.anchorStart = '0';
+        thiss.anchorEnd = '0';
         thiss.midPoints = []; // the logic part which instructs to generate the midnodes
         // if (!thiss.model && isDModelElementPointer(startid)) thiss.model = startid;
         // thiss.labels = undefined;
         let ll: labelfunc = (e: LVoidEdge, s: EdgeSegment, allNodes: LGraphElement[], allSegments: EdgeSegment[]
         ) => /*defining the edge label (e.start.model as any)?.name + " ~ " + (e.end.model as any)?.name */" (" + s.length.toFixed(1) + ")";
-        // this is the edge's label (thiss.longestLabel = ll)
+        // complex edge label func example: (thiss.longestLabel = ll)
+
         thiss.longestLabel = longestLabel;
         this.setPtr("start", startid);
         this.setPtr("end", endid);
@@ -826,7 +829,7 @@ export class Constructors<T extends DPointerTargetable = DPointerTargetable>{
     DRefEdge(): this { return this; }
 
     DGraphElement(model: DGraphElement["model"]|null|undefined, parentgraphID: DGraphElement["graph"]|undefined,
-                  htmlindex: number): this {
+                  htmlindex: number = 1): this {
         const thiss: DGraphElement = this.thiss as any;
         thiss.subElements = [];
         thiss.favoriteNode = false;
@@ -835,6 +838,7 @@ export class Constructors<T extends DPointerTargetable = DPointerTargetable>{
         thiss.edgesIn = [];
         thiss.edgesOut = [];
         thiss.state = {id: thiss.id+".state", className: thiss.className};
+        thiss.anchors = {'0':{x:0.5, y:0.5}, '1':{x:0.5, y:0}, '2':{x:1, y:0.5}, '3':{x:0.5, y:1}, '4':{x:0, y:0.5}} as any;
 
         this.setPtr("model", model);
         this.setPtr("graph", parentgraphID);
